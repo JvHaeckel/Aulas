232702eaffce1784d1db5a41b2f833293736dcfc
JvHaeckel <joaohaeckel@gmail.com>
2024-01-24 10:08:53 -0300

    SEBRAE

---
+++ B7Web/Modulo2/funcaoDentroObjeto.js
@@ -0,0 +1,18 @@
+/* https://alunos.b7web.com.br/curso/javascript/js-funcao-dentro-de-objeto 
+             Função dentro de objeto  */
+
+let person = {
+    name: 'João',
+    surname: 'Haeckel',
+    age: 27,
+    /* Criando a Function dentro do Objeto*/ 
+    fullName: function(){
+        return this.name + this.surname;
+        /* `${this.name} ${this.surname}`; */
+    }
+}
+
+console.log(pessoa.fullName());
+
+
+

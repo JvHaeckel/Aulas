dd2d8337dc5bf41234182aa7c1a0f94ad5990691
JvHaeckel <joaohaeckel@gmail.com>
2024-02-16 11:42:55 -0300

    SEBRAE

--- B7Web/DOM/Manipulando atributos.js
+++ B7Web/DOM/Manipulando atributos.js
@@ -1,45 +1,9 @@
-/* https://alunos.b7web.com.br/curso/javascript/js-manipulando-elementos-3*/
+/* https://alunos.b7web.com.br/curso/javascript/js-manipulando-atributos*/
+
 
 function clicou() {
 
-    const teste = document.querySelector('#teste');
-    const ul = teste.querySelector('ul');
-
-    /* Colocando elementos ao redor */
-    // ul.after("Texto depois");
-    // ul.before("Texto antes");
+    const input = document.querySelector('input');
 
 
-              /* Adicionando elemento de fato :*/
-
-    // let newButton = document.createElement('Button');
-    // newButton.innerHTML = 'Botão';
-    // ul.after(newButton);
-
-               /* Adicionando nova lista */
-
-    let newList = document.createElement('ul');
-
-    for (let i = 0; i < 5; i++) {
-        let newLi = document.createElement('li');
-        newLi.innerHTML = 'Item add' + (i + 1);
-        newList.append(newLi);
-    }
-
-    ul.append(newList);
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-

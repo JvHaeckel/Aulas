2c20bb52aba85b2acf5b488e787722c4d4fc94f8
JvHaeckel <joaohaeckel@gmail.com>
2024-02-20 10:31:27 -0300

    SEBRAE

---
+++ B7Web/DOM/Manipulando CSS.js
@@ -0,0 +1,23 @@
+/*  https://alunos.b7web.com.br/curso/javascript/js-manipulando-o-css  */
+
+// Manipulando o CSS
+
+function clicou() {
+
+   let li = document.querySelector('li');
+
+   li.style.backgroundColor = 'FFFF';
+   li.style.fontSize = '30px';
+
+}
+/* https://alunos.b7web.com.br/curso/javascript/js-trabalhando-com-classes  */
+// Manipulando o CSS com classes
+
+/* Ele apenas adicionou, removeu, substituiu classes usando o classList*/
+
+
+
+
+
+
+
--- B7Web/DOM/Manipulando atributos.js
+++ B7Web/DOM/Manipulando atributos.js
@@ -1,9 +1,35 @@
 /* https://alunos.b7web.com.br/curso/javascript/js-manipulando-atributos*/
 
+/*  Manipulando atributos parte 1  */
+
+// function clicou() {
+
+//     const input = document.querySelector('input');
+//     input.setAttribute('placeholder', 'Placeholder alterado');
+
+// }
+
+
+/* Manipulando atributos parte 2 : Ao clicar no button ele vai mostrar
+o que digitou e ao clicar de novo ele volta ao icógnito */
 
 function clicou() {
 
-    const input = document.querySelector('input');
+    const input = document.querySelector('input');  // antes de tudo vc seleciona o que vai modificar
+    let botao = document.querySelector('.botao');
+   
+    if(input.getAttribute('type') === 'text'){      
+        input.setAttribute('type', 'password');     // aqui setou a mudança de texto para password
+        botao.innerText = 'Mostrar Senha';
+    } else { 
+        input.setAttribute('type' , 'text')
+        botao.innerText = 'Ocultar Senha';
+    };  
+    
+    input.setAttribute('placeholder', 'Placeholder alterado');
 
 
 }
+
+
+

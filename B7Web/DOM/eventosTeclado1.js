676ee5aeb173d84df340bf1809981d1ad4501bed
JvHaeckel <joaohaeckel@gmail.com>
2024-02-20 10:56:23 -0300

    SEBRAE

---
+++ B7Web/DOM/eventosTeclado1.js
@@ -0,0 +1,29 @@
+// https://alunos.b7web.com.br/curso/javascript/javascript-eventos-de-teclado-1
+
+/*  Eventos de teclado 1   */
+
+// onkeydown  - quando aperta
+// onkeypress - quando está apertando
+// onkeyup    - quando solta
+
+// function apertou () {
+//     console.log("Apertou");
+// }
+
+// function segurou () { 
+//     console.log("Segurou");
+// }
+
+// function soltou () {
+//     console.log("Soltou");
+// }
+
+
+    /*   Eventos de teclado 2   */ 
+// https://alunos.b7web.com.br/curso/javascript/javascript-eventos-de-teclado-2
+
+
+
+
+
+

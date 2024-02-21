d2f23dea5f6d8be011ecab9ab95a81543d49fbef
JvHaeckel <joaohaeckel@gmail.com>
2024-02-21 11:53:07 -0300

    SEBRAE

--- B7Web/OrientaçãoObjetos/GetterSetter.js
+++ B7Web/OrientaçãoObjetos/GetterSetter.js
@@ -2,34 +2,29 @@
 class Person {
 
     // Para colocar variáveis dentro da classe não precisamos colocar let/const/var
+    _age = 0         // Colocar o Underline na variável que iremos pegar
     steps = 0;
-    salary = 0;
-
-    constructor (name,age){
+   
+    constructor (name){
         this.name = name;
-        this.age = age;
     }
 
      takeSteps (){
         this.steps ++;
      }
 
-     setSalary (valor){
-        this.valor ;
+     get age (){  // Retorna
+      return this._age;
      }
+    
 }
 
 let p1 = new Person('Joao', 26);
 let p2 = new Person('Maria', 20);
 let p3 = new Person('Gavi', 29);
 
-p1.salary = 1400;
-p2.salary = 1000;
-p3.salary = 3000;
-
 p1.takeSteps();
 
-console.log(`A pessoa: ${p1.name} tem ${p1.age} anos, com o salário de R$${p1.salary} e deu ${p1.steps} passo(s)`)
-
+console.log(`A pessoa: ${p1.name} tem ${p1.age} anos e deu ${p1.steps} passo(s)`)
 
 

208a63ab8c763825080b108f273c4958bbab0758
JvHaeckel <joaohaeckel@gmail.com>
2024-02-23 09:57:37 -0300

    SEBRAE

--- B7Web/POO/Heranca.js
+++ B7Web/POO/Heranca.js
@@ -8,20 +8,31 @@ class Person {
         this.name = name;
         this.lastName = lastName;
     }
+    sayHi (){
+        console.log(` OII, Meu nome é ${this.name} ${this.lastName}`);
+    }
 }
 
 class Student extends Person {  // Extends - a pessoa herda
     constructor(name, lastName, age, id) {
         super(name);
-        super(lastName);
+        this.lastName = lastName;
         this.age = age;
         this.id = id;
     }
+sayHello(){  /* Se colocasse o mesmo nome iria sobrescrever e essa seria levado em conta*/
+    super.sayHi(); // Super - vai ser usado para executar do pai(Person)
 }
 
-let p1 = new Student('joão', "Haeckel", 26, 171);
+}
 
-console.log(`Meu nome é ${p1.name} ${p1.lastName}, tenho ${p1.age} anos e matrícula ${p1.id}`);
+let p1 = new Student('João', "Haeckel", 26, 171);
+
+/* Antes de criar o método SayHi fiz assim: */
+// console.log(` ${p1.name} ${p1.lastName}, tenho ${p1.age} anos e matrícula ${p1.id}`);
+
+p1.sayHi();
+console.log(`, tenho ${p1.age} anos e matrícula ${p1.id}`);
 
 
 

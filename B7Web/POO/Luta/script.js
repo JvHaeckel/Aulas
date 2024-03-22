051609ba43d825b6c566c851b90b5f73e86de17f
JvHaeckel <joaohaeckel@gmail.com>
2024-03-22 10:13:29 -0300

    SEBRAE

--- B7Web/POO/Luta/script.js
+++ B7Web/POO/Luta/script.js
@@ -1,17 +1,26 @@
 
 class Character {
 
-life = 1;
-maxLife = 1;
-attack = 0;
-defense = 0;
+    life = 1;         // Tinha colocado sem o Underline mas tive que por pois é a regra quando usamos Getters e Setters
+    maxLife = 1;
+    attack = 0;
+    defense = 0;
 
-constructor (name){
-    this.name = name;
-}
+    constructor(name) {
+        this.name = name;
+    }
 
+    get life() {
+        return this._life
+    }
 
+    set life(newLife) {
+        this._life = new life < 0 ? 0 : newLife;
 
+        // if(newLife < 0 ){
+        //     this._life = 0;
+        // } else {  this._life = newLife};
+    }
 
 }
 

7fe2e86847bed685ae22d72c59be2fafeb11355e
JvHaeckel <joaohaeckel@gmail.com>
2024-07-08 09:08:36 -0300

    SEBRAE

---
+++ B7Web/Modulo 5/requisicoes.js
@@ -0,0 +1,30 @@
+/* https://alunos.b7web.com.br/curso/javascript/javascript-o-que-e-sincrono-e-assincrono*/ 
+
+// #2 O que é Síncrono e Assíncrono?
+
+
+// SÍNCRONO - ocorre na ordem do código
+
+let name = "João";
+let surname = "Haeckel";
+let nonbre = `${name} ${surname}` ;  /* O jeito que achei de separar o nome do sobrenome
+basta apenas colocar esse espacinho*/ 
+
+console.log(`O nome dele é ${nonbre}`);
+
+
+/* #3 Como a Web funciona (Requisição e Resposta)?
+
+
+ Requisição - tudo que acessa algo na internet. Vc envia vários dados
+ 
+ Cabeçalhos/ Headers : onde tem as infromações que ajuda a identificar quem pede a requisição
+ ex: id, ip, user
+ corpo/body: corpo da mensagem
+
+ Resposta - quando devolve as informações
+ 
+ Cabeçalhos/ Headers : onde tem as infromações que ajuda a identificar quem pede a requisição
+ ex: cache
+ corpo/body: corpo da mensagem
+ */

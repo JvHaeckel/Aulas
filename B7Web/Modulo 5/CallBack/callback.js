adde7561f2d1872e715432257920ec502982db5f
JvHaeckel <joaohaeckel@gmail.com>
2024-07-15 11:38:01 -0300

    SEBRAE

--- B7Web/Modulo 5/CallBack/callback.js
+++ B7Web/Modulo 5/CallBack/callback.js
@@ -3,4 +3,22 @@
 https://alunos.b7web.com.br/curso/javascript/javascript-o-que-e-um-callback */
 
 
-let botao = document.getElementById("Botão");
+// Se aplica a eventos, botoes e requisições. 
+
+// let botao = document.getElementById("Botão");
+// botao.onclick = function (){
+//     alert("Cuidado")
+// }
+
+document.getElementById("Botão").addEventListener('click', () => {
+    alert("Cuidado", clickCallBack);
+});
+
+let clickCallBack = ()=> alert("Cuidado");
+
+
+
+
+
+
+

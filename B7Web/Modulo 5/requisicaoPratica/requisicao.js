16657c3cfca3d4b1efe518b8a8fc58130a2f2b73
JvHaeckel <joaohaeckel@gmail.com>
2024-07-18 09:42:39 -0300

    SEBRAE

--- B7Web/Modulo 5/requisicaoPratica/requisicao.js
+++ B7Web/Modulo 5/requisicaoPratica/requisicao.js
@@ -1,4 +1,5 @@
 /* #6  Fazendo uma requisição na prática
+Até aula 11
 
 https://alunos.b7web.com.br/curso/javascript/javascript-fazendo-uma-requisicao-na-pratica
 
@@ -15,24 +16,42 @@ https://alunos.b7web.com.br/curso/javascript/javascript-fazendo-uma-requisicao-n
 /* */
 
 function clicou() {
-    fetch('https://jsonplaceholder.typicode.com/todos/1')
-      .then(response => response.json())
-      .then(json => console.log(json))
-        .catch()
+  fetch('https://jsonplaceholder.typicode.com/todos/1')
+    .then(response => response.json())  /* Converteu em Objeto*/
+    .then(json => console.log(json))
+    .catch()
 
-        
+
+}
+/* Resumi demais a parte do then em arrow function, mas foi o seguinte: apenas usamos a o fetch para fazer uma requisição 
+e depois tranformamos em JSON para termos um objeto de fato */
+
+
+// Na aula 11 fizemos uma requisição POST: 
+
+function inserir() {
+  fetch(
+    'https://jsonplaceholder.typicode.com/todos/1',  /* Foi criado um array dentro do fetch por isso as vírgulas*/
+    {
+      method: 'POST',
+      headers: {
+        'content-type': 'application/json'
+      },
+      body: JSON.stringify({            /* Essas informações estarão: na aba network - Payload*/
+        title: 'Título de teste',
+        body: 'Corpo de teste',
+        userId: 2
+      })      /* Essa função traduz o objeto em JSON*/
+
+
     }
+  ).then(response => response.json())  /* Converteu em Objeto*/
+  .then(json => console.log(json));
+}
 
 
 
-
-
-    /* Resumi demais a parte do then em arrow function, mas foi o seguinte:
-    apenas usamos a o fetch para fazer uma requisição e depois tranformamos
-    em JSON para termos um objeto de fato */
-
-    document.getElementById("Botão").addEventListener('click', clicou());
-
-
+document.getElementById("Botão").addEventListener('click', clicou);
+document.getElementById("inserir").addEventListener('click', inserir);
 
 
